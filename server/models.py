7c10388db5dd726e026af399c56194f554746fb3
Barasa <timbaros87@gmail.com>
2024-08-20 00:43:19 +0300

    REST Create and Retrieve

--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, default="")
+    price = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)
+    
+    def __init__ (self, name, image="", price=0.00):
+        self.name = name
+        self.image = image
+        self.price = price
